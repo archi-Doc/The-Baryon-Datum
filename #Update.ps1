bfda632507310c246c56fa58bcff7ee118694077
archi-Doc <archidoc422@gmail.com>
2025-03-13 20:36:53 +0900

    .

--- #Update.ps1
+++ #Update.ps1
@@ -1,21 +1,12 @@
 # Set-ExecutionPolicy RemoteSigned
 
-$result = git add . 2>&1
-if (-not $result)
-{
-    Write-Host 'git add : up-to-date'
-    #exit 0
-}
-else
-{
-    Write-Host $result
-}
+git add .
 
 $result = git commit -m "." 2>&1
 if ($result -match 'nothing to commit, working tree clean')
 {
     Write-Host 'git commit : up-to-date'
-    #exit 0
+    exit 0
 }
 else
 {
@@ -23,7 +14,6 @@ else
 }
 
 $result = git push 2>&1
-Write-Host $result
 if ($result -match 'Everything up-to-date')
 {
     Write-Host 'git push : up-to-date'
